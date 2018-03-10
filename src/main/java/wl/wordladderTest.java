b1849afc53dae7615f7c3be5c34eb590fa0e6c7f
yuki.h <yuki.h@sjtu.edu.cn>
2018-03-10 21:40:06 +0800

    correct .pom

--- src/main/java/wl/wordladderTest.java
+++ src/main/java/wl/wordladderTest.java
@@ -1,15 +1,13 @@
 package wl;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After;
 
 import java.io.ByteArrayInputStream;
 
-import static org.junit.Assert.*;
-
-
+import static org.junit.Assert.assertEquals;
 
 
 /** 
